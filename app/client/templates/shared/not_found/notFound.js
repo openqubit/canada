9c4746b779832f9fc7d5a62ba0c9b215727c10a9
openqubit <qubitsmith@gmail.com>
2016-08-22 02:41:34 +0300

    Update notFound.js

--- app/client/templates/shared/not_found/notFound.js
+++ app/client/templates/shared/not_found/notFound.js
@@ -42,6 +42,49 @@ geolocationError: function() {
 /* Home: Lifecycle Hooks */
 /*****************************************************************************/
 Template.NotFound.onCreated(function () {
+  
+   GoogleMaps.ready('map2', function(map) {
+      
+    $(window).resize(function() {
+        google.maps.event.trigger(map, 'resize');
+    });
+    google.maps.event.trigger(map, 'resize');
+    
+      
+      google.maps.event.addListener(map.instance, 'click', function(event) {
+        Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+      });
+    
+      var markers = {};
+
+      Markers.find().observe({
+        added: function (document) {
+          var marker = new google.maps.Marker({
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(document.lat, document.lng),
+            map: map.instance,
+            id: document._id
+          });
+
+          google.maps.event.addListener(marker, 'dragend', function(event) {
+            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+          });
+
+          markers[document._id] = marker;
+        },
+        changed: function (newDocument, oldDocument) {
+          markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+        },
+        removed: function (oldDocument) {
+          markers[oldDocument._id].setMap(null);
+          google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
+          delete markers[oldDocument._id];
+        }
+      });
+    });
+    
+    
  var self = this;
 
   GoogleMaps.ready('map', function(map) {
