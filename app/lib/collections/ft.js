fc6cc2558f090fd87b7dadff53da9f2d8208557f
openqubit <qubitsmith@gmail.com>
2016-08-22 03:29:15 +0300

    Update ft.js

--- app/lib/collections/ft.js
+++ app/lib/collections/ft.js
@@ -4,7 +4,7 @@ Uploads = new FS.Collection('uploads',{
   stores: [new FS.Store.FileSystem('uploads',{path:'~/projectUploads'})]
 });
 
-Markers = new Mongo.Collection('markers');
+Merkel = new Mongo.Collection('merkel');
 Pp = new Mongo.Collection('pp');
 Ft = new Mongo.Collection('ft');
 Af = new Mongo.Collection('af');
