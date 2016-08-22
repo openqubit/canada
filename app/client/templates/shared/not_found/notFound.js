62d40b3c93cf80ff0da2ddf7e108146405624cd0
openqubit <qubitsmith@gmail.com>
2016-08-22 03:42:03 +0300

    Update notFound.js

--- app/client/templates/shared/not_found/notFound.js
+++ app/client/templates/shared/not_found/notFound.js
@@ -35,7 +35,16 @@ geolocationError: function() {
         zoom: MAP_ZOOM
       };
     }
-  }	
+  },
+  map2Options: function() {
+      if (GoogleMaps.loaded()) {
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
+    }
+  });
 });
 
 /*****************************************************************************/
