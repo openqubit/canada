6f7cb5d93dcbac5ff6752ce92e2ed1e0e4442ea8
openqubit <qubitsmith@gmail.com>
2016-08-22 03:33:40 +0300

    Update publish.js

--- app/server/publish.js
+++ app/server/publish.js
@@ -2,6 +2,9 @@
     return Meteor.users.find({_id: this.userId}, {fields: {_id: 1, profile: 1, login: 1, email: 1}});
   });
 
+Meteor.publish('merkel', function () {
+  return Merkel.find();
+});
 
 Meteor.publish('ft', function () {
   return Ft.find();
