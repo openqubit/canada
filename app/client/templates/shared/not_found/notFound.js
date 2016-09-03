7f3cf9279a486252686fd97eff000536f5a9468d
openqubit <qubitsmith@gmail.com>
2016-09-03 17:50:07 +0300

    Update notFound.js

--- app/client/templates/shared/not_found/notFound.js
+++ app/client/templates/shared/not_found/notFound.js
@@ -4,9 +4,11 @@
 if (Meteor.isClient) {
   Meteor.startup(function() {
     console.log('hello');
+    /**
      GoogleMaps.load({
     key: 'AIzaSyD81kt-LoD3_Vqyqhd1yw9YlHq8J3SHpEg'
-  });
+    });
+    */
   });
 }
   
