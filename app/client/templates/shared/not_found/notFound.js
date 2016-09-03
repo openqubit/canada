c710df7650fa216f90a2d7892b10a2b516d21264
openqubit <qubitsmith@gmail.com>
2016-09-03 04:15:36 -0700

    Update notFound.js

--- app/client/templates/shared/not_found/notFound.js
+++ app/client/templates/shared/not_found/notFound.js
@@ -50,20 +50,27 @@ geolocationError: function() {
 /* Home: Lifecycle Hooks */
 /*****************************************************************************/
 Template.NotFound.onCreated(function () {
-  
-   GoogleMaps.ready('map2', function(map) {
-     
-     google.maps.event.trigger(map,'resize');
+
+
+  GoogleMaps.ready('map', function(map) {
+    
+    GoogleMaps.ready('map', function(map) {
+      
+    $(window).resize(function() {
+        google.maps.event.trigger(map, 'resize');
+    });
+    google.maps.event.trigger(map, 'resize');
+    
       
       google.maps.event.addListener(map.instance, 'click', function(event) {
-        Merkel.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+        Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
       });
     
       var markers = {};
 
-      Merkel.find().observe({
+      Markers.find().observe({
         added: function (document) {
-          var marker2 = new google.maps.Marker({
+          var marker = new google.maps.Marker({
             draggable: true,
             animation: google.maps.Animation.DROP,
             position: new google.maps.LatLng(document.lat, document.lng),
@@ -71,11 +78,11 @@ Template.NotFound.onCreated(function () {
             id: document._id
           });
 
-          google.maps.event.addListener(marker2, 'dragend', function(event) {
-            Merkel.update(marker2.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+          google.maps.event.addListener(marker, 'dragend', function(event) {
+            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
           });
 
-          markers[document._id] = marker2;
+          markers[document._id] = marker;
         },
         changed: function (newDocument, oldDocument) {
           markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
@@ -87,36 +94,7 @@ Template.NotFound.onCreated(function () {
         }
       });
     });
-    
-    
- var self = this;
-
-  GoogleMaps.ready('map', function(map) {
-    var marker;
-   google.maps.event.trigger(map,'resize');
-    // Create and move the marker when latLng changes.
-    self.autorun(function() {
-      var latLng = Geolocation.latLng();
-      if (! latLng)
-        return;
-
-      // If the marker doesn't yet exist, create it.
-      if (! marker) {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(latLng.lat, latLng.lng),
-          map: map.instance
-        });
-      }
-      // The marker already exists, so we'll just change its position.
-      else {
-        marker.setPosition(latLng);
-      }
-
-      // Center and zoom the map view onto the current position.
-      map.instance.setCenter(marker.getPosition());
-      map.instance.setZoom(MAP_ZOOM);
-    });
-  });		
+	
 });
 
 Template.NotFound.onRendered(function () {
