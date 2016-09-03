fe2baa32904a6e511e743cbebe5ea8fe705da448
openqubit <qubitsmith@gmail.com>
2016-09-03 16:22:56 +0300

    Update notFound.js

--- app/client/templates/shared/not_found/notFound.js
+++ app/client/templates/shared/not_found/notFound.js
@@ -18,16 +18,6 @@ geolocationError: function() {
     return error && error.message;
   },
   mapOptions: function() {
-    var latLng = Geolocation.latLng();
-    // Initialize the map once we have the latLng.
-    if (GoogleMaps.loaded() && latLng) {
-      return {
-        center: new google.maps.LatLng(latLng.lat, latLng.lng),
-        zoom: MAP_ZOOM
-      };
-    }
-  },
-  map2Options: function() {
       if (GoogleMaps.loaded()) {
         return {
           center: new google.maps.LatLng(-37.8136, 144.9631),
@@ -40,7 +30,6 @@ geolocationError: function() {
 
   
    Meteor.startup(function() {
-    var MAP_ZOOM = 15;
   GoogleMaps.load({
     key: 'AIzaSyD81kt-LoD3_Vqyqhd1yw9YlHq8J3SHpEg'
   });
