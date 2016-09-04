7a8e6c445a4f044eabc7aabc8e65d0dee3a06ec1
openqubit <qubitsmith@gmail.com>
2016-09-04 22:28:27 +0300

    Update ft.js

--- app/lib/collections/ft.js
+++ app/lib/collections/ft.js
@@ -708,18 +708,3 @@ Crowe.allow({
 });
 
 }
-if (Meteor.isClient) {
-  Markers.allow({
-    insert: function (userId, doc) {
-      return false;
-    },
-
-    update: function (userId, doc, fieldNames, modifier) {
-      return false;
-    },
-
-    remove: function (userId, doc) {
-      return false;
-    }
-  });
-}
