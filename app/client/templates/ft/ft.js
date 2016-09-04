0ccda57feca28462d0a8202af78de9f73eed80a9
openqubit <qubitsmith@gmail.com>
2016-09-04 18:33:39 +0300

    Update ft.js

--- app/client/templates/ft/ft.js
+++ app/client/templates/ft/ft.js
@@ -1,100 +1,54 @@
+Markers = new Mongo.Collection('markers');
 
-/*****************************************************************************/
-/* Ft: Event Handlers */
-/*****************************************************************************/
-Template.Ft.events({
-    'change .settings': function(event, template) {
+if (Meteor.isClient) {
+  Template.Ft.onCreated(function() {
+    GoogleMaps.ready('map', function(map) {
+      google.maps.event.addListener(map.instance, 'click', function(event) {
+        Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+      });
 
-    FS.Utility.eachFile(event,function(file){
+      var markers = {};
 
-        var fileObj=new FS.File(file);
+      Markers.find().observe({
+        added: function (document) {
+          var marker = new google.maps.Marker({
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(document.lat, document.lng),
+            map: map.instance,
+            id: document._id
+          });
 
-        Uploads.insert(fileObj,function(err){
-            
-            var lspath = fileObj.url({brokenIsFine: true});
-            var au = Meteor.absoluteUrl();
-            var au = au.replace(/\/$/, "");
-            localStorage.setItem("fp", lspath);
-            var ls = localStorage.getItem("fp");
-            $(".getdiscoveredimagepath").val(ls);                                 
-        });
+          google.maps.event.addListener(marker, 'dragend', function(event) {
+            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+          });
 
-        })
+          markers[document._id] = marker;
+        },
+        changed: function (newDocument, oldDocument) {
+          markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+        },
+        removed: function (oldDocument) {
+          markers[oldDocument._id].setMap(null);
+          google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
+          delete markers[oldDocument._id];
+        }
+      });
+    });
+  });
 
-  },
-// 	'click #sign-in-google': function(e, tmpl){
-//     Meteor.loginWithGoogle({
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
 
-//     }, function (err) {
-//         if (err){
-//             console.log("ERROR: " + err);//error handling
-//         } else {
-//             console.log("NO ERROR ON LOGIN");//show an alert
-//         }
-//     })
-// },
-'click #logout-button': function() {
-    var dp = JSON.parse(decodeURIComponent("%7B%22meteorRelease%22%3A%22METEOR%401.3.2.4%22%2C%22ROOT_URL%22%3A%22http%3A%2F%2F192.168.43.160%3A3000%2F%22%2C%22ROOT_URL_PATH_PREFIX%22%3A%22%22%2C%22DDP_DEFAULT_CONNECTION_URL%22%3A%22http%3A%2F%2F192.168.43.160%3A3000%2F%22%2C%22autoupdateVersionCordova%22%3A%22159c75af94f7671a4545a6d3d2cee2087ac5f69a%22%2C%22appId%22%3A%22rxyfxb1wa6cvsz7huua%22%2C%22meteorEnv%22%3A%7B%22NODE_ENV%22%3A%22production%22%2C%22TEST_METADATA%22%3A%22%7B%7D%22%7D%7D"));
-
-    console.log(dp);
-    // Meteor.logout();
-  }
-});
-
-
-/*****************************************************************************/
-/* Ft: Helpers */
-/*****************************************************************************/
-
-
-Template.Ft.helpers({
-    cdoc: function () {
-    return Af.findOne({"userid":"3R2pKdT3x9PjWLsD8"});
-    
-  },
-  discovery: function () {
-    return Pc.findOne({"userid":"3R2pKdT3x9PjWLsD8"});
-    
-  }
-});
-
-/*****************************************************************************/
-/* Ft: Lifecycle Hooks */
-/*****************************************************************************/
-Template.Ft.onCreated(function () {
-});
-
-Template.Ft.onRendered(function () {
-});
-
-Template.Ft.onDestroyed(function () {
-});
-Template.registerHelper('ex', function() {
-    var selector = {
-    userid: "3R2pKdT3x9PjWLsD8",
-    };
-         
-    var this_exists = Af.find(selector, {limit: 1}).count() > 0;
-
-   if(this_exists == true){
-        return true;
+  Template.Ft.helpers({
+    mapOptions: function() {
+      if (GoogleMaps.loaded()) {
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
     }
-    else{
-        return false;
-    }
-});
-
-Template.registerHelper('pc', function() {
-    var selector = {
-    userid: "3R2pKdT3x9PjWLsD8",
-    };
-         
-    var this_exists = Pc.find(selector, {limit: 1}).count() > 0;
-
-   if(this_exists == true){
-        return true;
-    }
-    else{
-        return false;
-    }
-});
+  });
+}
