c391a5a592dd039f211a3a35b2729e252a35724e
openqubit <qubitsmith@gmail.com>
2016-09-04 21:29:42 +0300

    Update map.js

--- app/client/templates/map/map.js
+++ app/client/templates/map/map.js
@@ -37,9 +37,14 @@ if (Meteor.isClient) {
     });
   });
 
+ if (Meteor.isClient) {
   Meteor.startup(function() {
-    GoogleMaps.load();
+    console.log('hello');
+    GoogleMaps.load({
+    key: 'AIzaSyD81kt-LoD3_Vqyqhd1yw9YlHq8J3SHpEg'
+    });
   });
+}
 
   Template.map.helpers({
     mapOptions: function() {
