4309c199a00ff0eb20ea0407fb5ee99552bb02e6
openqubit <qubitsmith@gmail.com>
2016-09-04 19:33:34 +0300

    Update map.js

--- app/client/templates/map/map.js
+++ app/client/templates/map/map.js
@@ -1,23 +1,54 @@
-/*****************************************************************************/
-/* Map: Event Handlers */
-/*****************************************************************************/
-Template.Map.events({
-});
+Markers = new Mongo.Collection('markers');
 
-/*****************************************************************************/
-/* Map: Helpers */
-/*****************************************************************************/
-Template.Map.helpers({
-});
+if (Meteor.isClient) {
+  Template.Map.onCreated(function() {
+    GoogleMaps.ready('map', function(map) {
+      google.maps.event.addListener(map.instance, 'click', function(event) {
+        Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+      });
 
-/*****************************************************************************/
-/* Map: Lifecycle Hooks */
-/*****************************************************************************/
-Template.Map.onCreated(function () {
-});
+      var markers = {};
 
-Template.Map.onRendered(function () {
-});
+      Markers.find().observe({
+        added: function (document) {
+          var marker = new google.maps.Marker({
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(document.lat, document.lng),
+            map: map.instance,
+            id: document._id
+          });
 
-Template.Map.onDestroyed(function () {
-});
+          google.maps.event.addListener(marker, 'dragend', function(event) {
+            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+          });
+
+          markers[document._id] = marker;
+        },
+        changed: function (newDocument, oldDocument) {
+          markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+        },
+        removed: function (oldDocument) {
+          markers[oldDocument._id].setMap(null);
+          google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
+          delete markers[oldDocument._id];
+        }
+      });
+    });
+  });
+
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+
+  Template.Map.helpers({
+    mapOptions: function() {
+      if (GoogleMaps.loaded()) {
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
+    }
+  });
+}
